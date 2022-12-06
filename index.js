84a6bb6ce6fe1cc958ff9faf597462aa1d0853e7
Dylan Abbott <dylanabbott33@yahoo.com>
2022-12-06 15:08:09 -0700

    commit tree 97b2664e7ff185dd6a613a810c7a56e25c5e76c9

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+      messages.push("Thank you, " + names[i] + ", for the wonderful " + event + " gift!")
+    }
+    return messages;
+  }
+
+  function countDown() {
+  var countDown = 9;
+    while (countDown >= 0)   {
+        console.log(countDown);
+        countDown = countDown - 1;
+    }
+    console.log(); }
